e2dabaed41a74759589a567d5f50e8099d39187e
OleksandrKirkov <kirkovsasha009@gmail.com>
2025-06-05 14:00:06 +0300

    feat: update tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+    content: [
+        './src/**/*.{html,js,ts,tsx,jsx,css,mdx}',
+        './src/components/**/*.{html,js,ts,tsx,jsx,css,mdx}',
+    ],
+    plugins: [],
+    theme: {},
+}
+
+export default config
