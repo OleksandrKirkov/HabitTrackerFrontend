717899df4a0358bb12f15ccfa3737196b13f6ea7
OleksandrKirkov <kirkovsasha009@gmail.com>
2025-06-09 13:27:34 +0300

    feat: update colors in habit-card component

--- src/components/screens/home/habit-card/habit-card.tsx
+++ src/components/screens/home/habit-card/habit-card.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 
 import { NotificationIcon } from '@/components/svg'
 import { HabitType } from '@/types/habit.type'
+import { getColorMap } from '@/utils/get-color-map'
 
 import { DayItem } from './card-value'
 
@@ -9,12 +10,12 @@ const days: string[] = ['Tue', 'Mon', 'Sun', 'Sat', 'Fri', 'Thu', 'Wed']
 
 export function HabitCard(props: HabitType) {
     return (
-        <Link href={`/habits/${props.id}`} className='block p-4 bg-secondary rounded-2xl w-full'>
+        <Link href={`/habit/?id=${props.id}`} className='block p-4 bg-secondary rounded-2xl w-full'>
             <div className='flex items-center gap-4 justify-between mb-4'>
                 <p className='font-bold leading-none text-text'>{props.title}</p>
                 <div className='flex items-center gap-2'>
                     <p className='text-xs text-text'>{props.frequency.type}</p>
-                    <NotificationIcon className={`w-4 h-4 stroke-yellow`} />
+                    <NotificationIcon className={`w-4 h-4 ${getColorMap('stroke', props.color)}`} />
                 </div>
             </div>
             <div className='flex items-center gap-1 justify-between w-full'>
@@ -25,7 +26,7 @@ export function HabitCard(props: HabitType) {
                         value={index}
                         is_complete={false}
                         is_today={false}
-                        color='yellow'
+                        color={props.color}
                     />
                 ))}
             </div>
