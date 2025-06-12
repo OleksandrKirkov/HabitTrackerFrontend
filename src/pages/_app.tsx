3e4c4437f6d56bdff9647015f9e1b34a72a8cc8c
OleksandrKirkov <kirkovsasha009@gmail.com>
2025-06-12 13:09:43 +0300

    feat: create store config and add provider in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import '@/styles/globals.css'
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import { ReactElement, ReactNode } from 'react'
+import { Provider } from 'react-redux'
+
+import { store } from '@/api/store/store'
 
 export type NextPageWithLayout<P = object> = NextPage<P> & {
     getLayout?: (page: ReactElement) => ReactNode
@@ -15,5 +18,5 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
     const getLayout = Component.getLayout ?? ((page) => page)
 
-    return getLayout(<Component {...pageProps} />)
+    return <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
 }
