f88fa47a6d9b9949f6e0ae0f81f598eb405ee1aa
OleksandrKirkov <kirkovsasha009@gmail.com>
2025-06-12 12:48:38 +0300

    chore: update text field styles

--- src/components/ui/text-field/text-field.tsx
+++ src/components/ui/text-field/text-field.tsx
@@ -3,7 +3,7 @@ import { InputHTMLAttributes } from 'react'
 export function TextField({ className, ...props }: InputHTMLAttributes<HTMLInputElement>) {
     return (
         <input
-            className={`bg-primary shadow-card w-full h-12 rounded-2xl px-4 placeholder:text-text ${className}`}
+            className={`bg-primary shadow-card w-full h-12 rounded-2xl px-4 outline-none text-white placeholder:text-text ${className}`}
             {...props}
         />
     )
